6ef85afeadc5968b17ea335934d03d9788e671b1
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-10-12 03:16:53 +0300

    basic annotations add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def add(a: float, b: float) -> float:
+    """
+    add: functions add two floats
+    a: first number
+    b: second number
+    return
+    """
+    return (a + b)
