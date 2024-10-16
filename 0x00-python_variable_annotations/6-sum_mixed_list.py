c9264ffa9f7c0297598efc569c5dd606810343d6
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-10-16 13:06:33 +0300

    typo

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,9 +2,9 @@
 """
 Complex types - mixed list
 """
+from typing import List
 
-
-def sum_mixed_list(mxd_lst: [float, int]) -> float:
+def sum_mixed_list(mxd_lst: List[float, int]) -> float:
     """
     finds sums in a list and return a float
     """
