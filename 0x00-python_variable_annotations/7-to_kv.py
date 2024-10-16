7ee51016d5b0df0af57d0b2062d195f3c2540597
MK-Aaron <mwendwa_aaron78@yahoo.com>
2024-10-16 13:32:20 +0300

    Tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Complex types - string and int/float to tuple
+"""
+from typing import Union, List, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Creates a tuple
+    """
+    return [k, v*v]
